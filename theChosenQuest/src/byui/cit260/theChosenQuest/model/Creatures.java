05a99b212dd14e217e1a9fad897094c8b9f41db5
Ryan Daniel Webb <rwebb626@me.com>
2016-05-12 18:00:40 -0700

    Creatures Class created

--- theChosenQuest/src/byui/cit260/theChosenQuest/model/Creatures.java
+++ theChosenQuest/src/byui/cit260/theChosenQuest/model/Creatures.java
@@ -5,10 +5,97 @@
  */
 package byui.cit260.theChosenQuest.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Zack Durbin
  */
-public class Creatures {
+public class Creatures implements Serializable{
+    
+    // class instance variables
+    private int hp;
+    private int attack;
+    private int defense;
+    private String name;
+
+    public Creatures() {
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.hp;
+        hash = 97 * hash + this.attack;
+        hash = 97 * hash + this.defense;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Creatures other = (Creatures) obj;
+        if (this.hp != other.hp) {
+            return false;
+        }
+        if (this.attack != other.attack) {
+            return false;
+        }
+        if (this.defense != other.defense) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Creatures{" + "hp=" + hp + ", attack=" + attack + ", defense=" + defense + ", name=" + name + '}';
+    }
+    
     
 }
