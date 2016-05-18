1492313598e307f1df55b4bf32d18d23d712214c
zdurbin <dur15014@byui.edu>
2016-05-18 10:39:31 -0700

    Added checkHealth controller class

---
+++ theChosenQuest/src/byui/cit260/theChosenQuest/control/checkHealth.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theChosenQuest.control;
+
+/**
+ *
+ * @author Zack Durbin
+ */
+public class checkHealth {
+    
+}
+//This controller will inform the user of their health after attacks have been done. If hp <= 0 Game is over.
+
