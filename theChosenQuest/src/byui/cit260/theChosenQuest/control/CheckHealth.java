1393e756f36ab8b22babbc1afad3a967ce030b08
Ryan Webb <rwebb626@me.com>
2016-05-28 18:06:22 -0700

    Updating Check Health Code

--- theChosenQuest/src/byui/cit260/theChosenQuest/control/CheckHealth.java
+++ theChosenQuest/src/byui/cit260/theChosenQuest/control/CheckHealth.java
@@ -10,7 +10,28 @@ package byui.cit260.theChosenQuest.control;
  * @author Zack Durbin
  */
 public class CheckHealth {
-    
+    public int checkhealth (int health, boolean battle, int attack, boolean rest) {
+        
+        // Perform Sanity Checks
+        if ((battle == false) && (rest == false)) {
+            return -1;
+        }
+        
+        if ((battle == true)&& (rest == true)) {
+            return -1;
+        }
+        
+        if (battle == true) {
+            if (attack >= 0) {
+            health -= attack;
+        }
+        }
+        if (rest == true) {
+            health = 100;
+        }
+        
+        return -1;
+    }
 }
 //This controller will inform the user of their health after attacks have been done. If hp <= 0 Game is over.
 
