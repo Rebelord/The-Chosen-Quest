5a4423b0a1a12aad696cd0d1c2e9daaecec0b29a
Ryan Daniel Webb <rwebb626@me.com>
2016-07-17 21:33:44 -0700

    Fixed Error
    Equipment model class need to be re-setup

--- theChosenQuest/src/byui/cit260/theChosenQuest/model/Equipment.java
+++ theChosenQuest/src/byui/cit260/theChosenQuest/model/Equipment.java
@@ -6,21 +6,24 @@
 package byui.cit260.theChosenQuest.model;
 
 import java.io.Serializable;
-
-
+import java.util.Objects;
 
 /**
  *
  * @author Zack Durbin
  */
 public class Equipment implements Serializable {
-   
+
     private String name;
     private int attack;
     private int defense;
     private int cost;
     private String type;
     private Boolean equiped;
+
+    public Equipment() {
+
+    }
 
     public String getName() {
         return name;
@@ -69,5 +72,64 @@ public class Equipment implements Serializable {
     public void setEquiped(Boolean equiped) {
         this.equiped = equiped;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + this.attack;
+        hash = 37 * hash + this.defense;
+        hash = 37 * hash + this.cost;
+        hash = 37 * hash + Objects.hashCode(this.type);
+        hash = 37 * hash + Objects.hashCode(this.equiped);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equipment other = (Equipment) obj;
+        if (this.attack != other.attack) {
+            return false;
+        }
+        if (this.defense != other.defense) {
+            return false;
+        }
+        if (this.cost != other.cost) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.equiped, other.equiped)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Equipment(String name, int attack, int defense, int cost, String type, Boolean equiped) {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.cost = cost;
+        this.type = type;
+        this.equiped = equiped;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" + "name=" + name + ", attack=" + attack + ", defense=" + defense + ", cost=" + cost + ", type=" + type + ", equiped=" + equiped + '}';
+    }
+
 }
